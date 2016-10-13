f7b1028530e5e00e9ec8a6d99119c5275623c6a9
Saurabh Kumar <saurabhkumar.spsu@gmail.com>
2016-10-13 19:01:31 -0400

    Update main.py

--- twitterFeedExtractor/main.py
+++ twitterFeedExtractor/main.py
@@ -1,34 +1,17 @@
-#Twitter Handles used to create the Twitter Database
+# The main function to store tweets into database
+
+# Importing the saveTweets function files sub folder 
 from files.db_query import saveTweets
-twitter_account = ["@SarahJindra",
-"@wazetrafficchi",
-"@GetRaasta",
-"@dtptraffic",
-"@TrafflineDEL",
-"@TrafflineCHN",
-"@TrafflineIndore",
-"@TotalTrafficCHI",
-"@TfL",
-"@HighwaysSEAST",
-"@BillWest5",
-"@John_Kass",
-"@iKartikRao",
-"@shelvieana_prat",
-"@prats_09",
-"@94_294_tollway",
-"@DildineMedia",
-"@CrazyRicardo",
-"@TotalTrafficNYC",
-"@WazeTrafficNYC",
-"@Traffic4NY",
-"@NYTrafficAlert",
-"@NYC_DOT",
-"@511NYC",
-"@NYPD_Traffic",
-"@NYCityAlerts",
-"@TotalTrafficNYC",
-]
+#Twitter Handles used to create the Twitter Database
+twitter_account = ["@SarahJindra","@wazetrafficchi","@GetRaasta","@dtptraffic","@TrafflineDEL","@TrafflineCHN","@TrafflineIndore",
+"@TotalTrafficCHI","@TfL","@HighwaysSEAST","@BillWest5","@John_Kass","@iKartikRao","@shelvieana_prat","@prats_09","@94_294_tollway",
+"@DildineMedia","@CrazyRicardo","@TotalTrafficNYC","@WazeTrafficNYC","@Traffic4NY","@NYTrafficAlert","@NYC_DOT","@511NYC","@NYPD_Traffic",
+"@NYCityAlerts","@TotalTrafficNYC",]
+
+#A number of tweets that we got whenever the API was hit.
+#Note : 200 is the max limit to for free token
 numbr_of_tweets = 200
+
 for twitter_id in twitter_account:
     print "\n"
     print "Saving Data for",twitter_id
