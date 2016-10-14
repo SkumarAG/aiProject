ab49003eec3ccdd3114f1b8352d48f370ae9f802
Saurabh Kumar <saurabhkumar.spsu@gmail.com>
2016-10-13 20:15:29 -0400

    Delete data_read.py

--- bagofwords/data_read.py
+++
@@ -1,6 +0,0 @@
-from pandas import read_csv
-from collections import OrderedDict
-import re,itertools
-def dataRead():
-  tweet_data = read_csv(filepath_or_buffer ="C:/Users/Ramanuja/Desktop/data2.csv",header=None,usecols = [9])# since no header info
-  return tweet_data
